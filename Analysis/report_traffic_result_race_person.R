0bdec9f12138607840199e18533930efe5cee69a
Jennifer Zhang <JZhang@catalystcalifornia.org>
2024-09-16 15:30:42 -0700

    Finishing analysis of traffic stops by stop result. Created new script for next analysis looking at rate of traffic stops by result cut by race.

---
+++ Analysis/report_traffic_result_race_person.R
@@ -0,0 +1,4 @@
+# Analysis 2: Results of traffic stops by race ----------------------------------
+# Include rate out of each racial group and rate out of each result type (e.g., % of Latinx stops resulting in a warning and % of warning stops that involved Latinx person)
+
+
---
+++ Analysis/report_traffic_result_stop.R
@@ -0,0 +1,80 @@
+###Analysis: Traffic stops by result of stop and result code. Includes a cut by race. 
+
+#Set up work space---------------------------------------
+
+
+library(RPostgreSQL)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+#connect to postgres
+
+source("W:\\RDA Team\\R\\credentials_source.R")
+
+con <- connect_to_db("eci_fresno_ripa")
+
+# pull in necessary analysis tables
+
+stop<-dbGetQuery(con, "SELECT * FROM rel_stops")
+stop_reason<-dbGetQuery(con, "SELECT * FROM rel_stops_reason")
+stop_result<-dbGetQuery(con, "SELECT * FROM rel_stops_result")
+stop_race<-dbGetQuery(con, "SELECT * FROM rel_stops_race")
+
+person<-dbGetQuery(con, "SELECT * FROM rel_persons")
+person_race<-dbGetQuery(con, "SELECT * FROM report_traffic_result_stop")
+
+# Analysis 1:  Traffic stops by result ---------------------------
+# General table of people stopped for traffic violations by simple result
+
+
+# Take stop table and filter for calls for service, then join all needed tables
+
+df<-stop%>%
+  filter(call_for_service==0)%>%
+  left_join(stop_reason)%>%
+  left_join(stop_result)
+
+# Analyze
+
+df<-df%>%
+  filter(stop_reason_simple=="Traffic violation")%>%
+  mutate(total=n())%>%
+  group_by(stop_result_simple)%>%
+  mutate(count=n(),
+         rate=count/total*100)%>%
+  slice(1)%>%
+  select(stop_reason_simple, stop_result_simple, total, count, rate)%>%
+  arrange(-rate)
+
+# Push table to postgres------------------------------
+
+# set column types
+charvect = rep('varchar', ncol(df)) 
+charvect <- replace(charvect, c(3,4,5), c("numeric"))
+
+# add df colnames to the character vector
+
+names(charvect) <- colnames(df)
+
+dbWriteTable(con,  "report_traffic_result_stop", df,
+             overwrite = TRUE, row.names = FALSE,
+             field.types = charvect)
+
+
+# # write comment to table, and column metadata
+
+table_comment <- paste0("COMMENT ON TABLE report_traffic_result_stop  IS 'Analyzing officer-initiated traffic stops by simple stop result.
+R script used to recode and import table: W:\\Project\\ECI\\Fresno RIPA\\GitHub\\JZ\\fresnoripa\\Analysis\\report_traffic_result_stop.R
+QA document: W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_report_traffic_result_stop.docx';
+
+COMMENT ON COLUMN report_traffic_result_stop.stop_reason_simple IS 'Reason for stop (which will only be traffic violations for this analysis)';
+COMMENT ON COLUMN report_traffic_result_stop.stop_result_simple IS 'Simple reason for stop';
+COMMENT ON COLUMN report_traffic_result_stop.total IS 'Total number of officer-initiated traffic stops (denominator in rate calc)';
+COMMENT ON COLUMN report_traffic_result_stop.count IS 'Count of officer-initiated traffic stops for each stop result (numerator for rate calc)';
+COMMENT ON COLUMN report_traffic_result_stop.rate IS 'Rate of officer-initiated traffic stops by stop result';
+")
+
+# send table comment + column metadata
+ dbSendQuery(conn = con, table_comment)
+
