6c8251a1b1fc98b1dcbef93f104286d2c1b39327
Jennifer Zhang <JZhang@catalystcalifornia.org>
2024-09-24 11:03:26 -0700

    renaming the 'report_traffic_result_race' table and R filename to make it slightly shorter

--- Analysis/report_traffic_result_race.R
+++ Analysis/report_traffic_result_race.R
@@ -279,26 +279,26 @@ charvect <- replace(charvect, c(5,6,7,8), c("numeric"))
 
 names(charvect) <- colnames(df)
 
-dbWriteTable(con,  "report_traffic_result_race_person", df,
+dbWriteTable(con,  "report_traffic_result_race", df,
              overwrite = TRUE, row.names = FALSE,
              field.types = charvect)
 
 
 # # write comment to table, and column metadata
 
-table_comment <- paste0("COMMENT ON TABLE report_traffic_result_race_person  IS 'Analyzing officer-initiated traffic stops by simple stop result by race. This table includes rates using three different denominators 1) out of all traffic stops witin each racial group 2) out of all traffic stop results 3) per 1k of the population of each racial group in Fresno city.
+table_comment <- paste0("COMMENT ON TABLE report_traffic_result_race  IS 'Analyzing officer-initiated traffic stops by simple stop result by race. This table includes rates using three different denominators 1) out of all traffic stops witin each racial group 2) out of all traffic stop results 3) per 1k of the population of each racial group in Fresno city.
 The denominator is indicated by the denom column in the table. 
-R script used to recode and import table: W:\\Project\\ECI\\Fresno RIPA\\GitHub\\JZ\\fresnoripa\\Analysis\\report_traffic_result_race_person.R
-QA document: W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_report_traffic_result_race_person.docx';
+R script used to recode and import table: W:\\Project\\ECI\\Fresno RIPA\\GitHub\\JZ\\fresnoripa\\Analysis\\report_traffic_result_race.R
+QA document: W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_report_traffic_result_race.docx';
 
-COMMENT ON COLUMN report_traffic_result_race_person.race IS 'Perceived race';
-COMMENT ON COLUMN report_traffic_result_race_person.reason IS 'Reason for stop (which will only be traffic violations for this analysis)';
-COMMENT ON COLUMN report_traffic_result_race_person.denom IS 'Denominator used in analysis';
-COMMENT ON COLUMN report_traffic_result_race_person.stop_result_simple IS 'Simple result for stop';
-COMMENT ON COLUMN report_traffic_result_race_person.total IS 'Total number (denominator in rate calc) see denom column for which denominator is used';
-COMMENT ON COLUMN report_traffic_result_race_person.count IS 'Count of officer-initiated traffic stops for each stop result for each racial group (numerator for rate calc)';
-COMMENT ON COLUMN report_traffic_result_race_person.rate IS 'Rate of traffic stop results by race: count/total*100 (see denom colunm for more clarity on which demominator is being used to create the rate)';
-COMMENT ON COLUMN report_traffic_result_race_person.rate_per_1k IS 'Rate of traffic stops by result of stop for each perceived racial group per 1k total population of each racial gorup in Fresno city';
+COMMENT ON COLUMN report_traffic_result_race.race IS 'Perceived race';
+COMMENT ON COLUMN report_traffic_result_race.reason IS 'Reason for stop (which will only be traffic violations for this analysis)';
+COMMENT ON COLUMN report_traffic_result_race.denom IS 'Denominator used in analysis';
+COMMENT ON COLUMN report_traffic_result_race.stop_result_simple IS 'Simple result for stop';
+COMMENT ON COLUMN report_traffic_result_race.total IS 'Total number (denominator in rate calc) see denom column for which denominator is used';
+COMMENT ON COLUMN report_traffic_result_race.count IS 'Count of officer-initiated traffic stops for each stop result for each racial group (numerator for rate calc)';
+COMMENT ON COLUMN report_traffic_result_race.rate IS 'Rate of traffic stop results by race: count/total*100 (see denom colunm for more clarity on which demominator is being used to create the rate)';
+COMMENT ON COLUMN report_traffic_result_race.rate_per_1k IS 'Rate of traffic stops by result of stop for each perceived racial group per 1k total population of each racial gorup in Fresno city';
 ")
 
 # send table comment + column metadata
