0e6e6252276c30b7464e1e887cf6f8c2a63b106f
Jennifer Zhang <JZhang@catalystcalifornia.org>
2024-10-08 16:28:08 -0700

    potentially adding new analysis table for traffic citation results by race WITHOUT traffic stop type

--- Analysis/report_citation_traffic_type_stop.R
+++ Analysis/report_citation_traffic_type_stop.R
@@ -49,7 +49,7 @@ df1<-df%>%
          rate=count/total*100)%>%
   slice(1)%>%
   ungroup()%>%
-  select(traffic_violation_type, rfs_traffic_violation_code, statute_literal_25, total, count, rate)%>%
+  select(traffic_violation_type, statute_literal_25, total, count, rate)%>%
   arrange(traffic_violation_type, -rate)%>%
   group_by(traffic_violation_type)%>%
   slice(1:5)
@@ -77,7 +77,6 @@ R script used to recode and import table: W:\\Project\\ECI\\Fresno RIPA\\GitHub\
 QA document: W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_report_citation_traffic_type_stop.docx';
 
 COMMENT ON COLUMN report_citation_traffic_type_stop.traffic_violation_type IS 'Traffic stop type (moving, nonmoving, equipment)';
-COMMENT ON COLUMN report_citation_traffic_type_stop.rfs_traffic_violation_code IS 'Traffic stop resulting in a citation specific citation code';
 COMMENT ON COLUMN report_citation_traffic_type_stop.statute_literal_25 IS 'Text description for accompnaying offense code';
 COMMENT ON COLUMN report_citation_traffic_type_stop.total IS 'Total number (denominator in rate calc) of traffic stops that resulted in a citation within each traffic stop type';
 COMMENT ON COLUMN report_citation_traffic_type_stop.count IS 'Count of each specific citation offense code within each traffic stop type';
@@ -110,7 +109,7 @@ df_nh<-df%>%
          rate=count/total*100)%>%
   slice(1)%>%
   ungroup()%>%
-  select(nh_race, traffic_violation_type,  rfs_traffic_violation_code, statute_literal_25,
+  select(nh_race, traffic_violation_type, statute_literal_25,
          total, count, rate)%>%
   arrange(nh_race, traffic_violation_type, -rate)%>%
   group_by(nh_race, traffic_violation_type)%>%
@@ -130,7 +129,7 @@ df_aian<-df%>%
   slice(1)%>%
   mutate(nh_race="aian_aoic")%>%
   ungroup()%>%
-  select(nh_race, traffic_violation_type, rfs_traffic_violation_code, statute_literal_25,  total, count, rate)%>%
+  select(nh_race, traffic_violation_type, statute_literal_25,  total, count, rate)%>%
   arrange(nh_race, traffic_violation_type, -rate)
 
 #### NHPI ####
@@ -147,7 +146,7 @@ df_nhpi<-df%>%
   slice(1)%>%
 ungroup()%>%
   mutate(nh_race="nhpi_aoic")%>%
-  select(nh_race, traffic_violation_type, rfs_traffic_violation_code, statute_literal_25,  total, count, rate)%>%
+  select(nh_race, traffic_violation_type, statute_literal_25,  total, count, rate)%>%
   arrange(nh_race, traffic_violation_type, -rate)
 
 
@@ -165,27 +164,27 @@ df_sswana<-df%>%
   slice(1)%>%
 ungroup()%>%
   mutate(nh_race="sswana_aoic")%>%
-  select(nh_race, traffic_violation_type, rfs_traffic_violation_code, statute_literal_25,  total, count, rate)%>%
+  select(nh_race, traffic_violation_type, statute_literal_25,  total, count, rate)%>%
   arrange(nh_race, traffic_violation_type, -rate)%>%
   group_by(nh_race, traffic_violation_type)%>%
   slice(1:5)
 
 #### Final combine of all race tables ####
 
-df_race<-rbind(df_nh, df_aian, df_nhpi, df_sswana)%>%
+df_race_type<-rbind(df_nh, df_aian, df_nhpi, df_sswana)%>%
   rename("race"="nh_race")
 
 # Push RACE + STOP TYPE table to postgres----------------------------------------
 
 # set column types
-charvect = rep('varchar', ncol(df_race)) 
+charvect = rep('varchar', ncol(df_race_type)) 
 charvect <- replace(charvect, c(5,6,7), c("numeric"))
 
 # add df colnames to the character vector
 
-names(charvect) <- colnames(df_race)
+names(charvect) <- colnames(df_race_type)
 
-dbWriteTable(con,  "report_citation_traffic_type_race", df_race,
+dbWriteTable(con,  "report_citation_traffic_type_race", df_race_type,
              overwrite = TRUE, row.names = FALSE,
              field.types = charvect)
 
@@ -201,7 +200,6 @@ QA document: W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_report_citation_tr
 
 COMMENT ON COLUMN report_citation_traffic_type_race.race IS 'Perceived race';
 COMMENT ON COLUMN report_citation_traffic_type_race.traffic_violation_type IS 'Traffic stop type (moving, nonmoving, equipment)';
-COMMENT ON COLUMN report_citation_traffic_type_race.rfs_traffic_violation_code IS 'Traffic stop resulting in a citation specific citation code';
 COMMENT ON COLUMN report_citation_traffic_type_race.statute_literal_25 IS 'Text description for accompnaying offense code';
 COMMENT ON COLUMN report_citation_traffic_type_race.total IS 'Total number (denominator in rate calc) of traffic stops that resulted in a citation within each traffic stop type';
 COMMENT ON COLUMN report_citation_traffic_type_race.count IS 'Count of each specific citation offense code within each traffic stop type';
@@ -214,63 +212,58 @@ dbSendQuery(conn = con, table_comment)
 
 # EXPLORATION: Analysis by race WITHOUT stop type: -----------------------------------------------
 
-# JZ: I am not sure how useful this is but am going to push to postgres to explore more
-
-# Rate: Percent of citation offense code results out of all citation results within each traffic type for each race
-# i.e.) number offense code A for perceived Latinx person in non-moving stop / all citations of Latinx people in non-moving stops
+# Rate: Percent of citation offense code results out of all citation results within each race
+# i.e.) number offense code A for perceived Latinx person in non-moving stop / all citations of Latinx people
 
 #### NH ####
 
 df_nh<-df%>%
   left_join(person_race)%>%
   filter(stop_result_simple=="citation for infraction")%>%
-  group_by(traffic_violation_type, nh_race)%>%
+  group_by(nh_race)%>%
   mutate(total=n())%>%
   left_join(offense_codes, by=c("rfs_traffic_violation_code"="offense_code"))%>%
-  group_by(traffic_violation_type, statute_literal_25, offense_type_of_charge, nh_race)%>%
+  group_by(statute_literal_25, offense_type_of_charge, nh_race)%>%
   mutate(count=n(),
          rate=count/total*100)%>%
   slice(1)%>%
   ungroup()%>%
-  select(nh_race, traffic_violation_type,  rfs_traffic_violation_code, statute_literal_25,
+  select(nh_race, statute_literal_25,
          total, count, rate)%>%
-  arrange(nh_race, traffic_violation_type, -rate)%>%
-  group_by(nh_race, traffic_violation_type)%>%
-  slice(1:5)
+  arrange(nh_race, -rate)%>%
+  group_by(nh_race)
 
 #### AIAN ####
 
 df_aian<-df%>%
   left_join(person_race)%>%
   filter(stop_result_simple=="citation for infraction" & aian_flag == 1)%>%
-  group_by(traffic_violation_type)%>%
   mutate(total=n())%>%
   left_join(offense_codes, by=c("rfs_traffic_violation_code"="offense_code"))%>%
-  group_by(traffic_violation_type, statute_literal_25, offense_type_of_charge)%>%
+  group_by(statute_literal_25, offense_type_of_charge)%>%
   mutate(count=n(),
          rate=count/total*100)%>%
   slice(1)%>%
   mutate(nh_race="aian_aoic")%>%
   ungroup()%>%
-  select(nh_race, traffic_violation_type, rfs_traffic_violation_code, statute_literal_25,  total, count, rate)%>%
-  arrange(nh_race, traffic_violation_type, -rate)
+  select(nh_race, statute_literal_25,  total, count, rate)%>%
+  arrange(nh_race, -rate)
 
 #### NHPI ####
 
 df_nhpi<-df%>%
   left_join(person_race)%>%
   filter(stop_result_simple=="citation for infraction" & nhpi_flag == 1)%>%
-  group_by(traffic_violation_type)%>%
   mutate(total=n())%>%
   left_join(offense_codes, by=c("rfs_traffic_violation_code"="offense_code"))%>%
-  group_by(traffic_violation_type, statute_literal_25, offense_type_of_charge)%>%
+  group_by(statute_literal_25, offense_type_of_charge)%>%
   mutate(count=n(),
          rate=count/total*100)%>%
   slice(1)%>%
   ungroup()%>%
   mutate(nh_race="nhpi_aoic")%>%
-  select(nh_race, traffic_violation_type, rfs_traffic_violation_code, statute_literal_25,  total, count, rate)%>%
-  arrange(nh_race, traffic_violation_type, -rate)
+  select(nh_race, statute_literal_25,  total, count, rate)%>%
+  arrange(nh_race, -rate)
 
 
 #### SSWANA ####
@@ -278,23 +271,56 @@ df_nhpi<-df%>%
 df_sswana<-df%>%
   left_join(person_race)%>%
   filter(stop_result_simple=="citation for infraction" & sswana_flag == 1)%>%
-  group_by(traffic_violation_type)%>%
   mutate(total=n())%>%
   left_join(offense_codes, by=c("rfs_traffic_violation_code"="offense_code"))%>%
-  group_by(traffic_violation_type, statute_literal_25, offense_type_of_charge)%>%
+  group_by( statute_literal_25, offense_type_of_charge)%>%
   mutate(count=n(),
          rate=count/total*100)%>%
   slice(1)%>%
   ungroup()%>%
   mutate(nh_race="sswana_aoic")%>%
-  select(nh_race, traffic_violation_type, rfs_traffic_violation_code, statute_literal_25,  total, count, rate)%>%
-  arrange(nh_race, traffic_violation_type, -rate)%>%
-  group_by(nh_race, traffic_violation_type)%>%
-  slice(1:5)
+  select(nh_race, statute_literal_25,  total, count, rate)%>%
+  arrange(nh_race, -rate)%>%
+  group_by(nh_race)
 
 #### Final combine of all race tables ####
 
 df_race<-rbind(df_nh, df_aian, df_nhpi, df_sswana)%>%
   rename("race"="nh_race")
+
+# Push RACE table to postgres----------------------------------------
+
+# set column types
+charvect = rep('varchar', ncol(df_race)) 
+charvect <- replace(charvect, c(5,6,7), c("numeric"))
+
+# add df colnames to the character vector
+
+names(charvect) <- colnames(df_race)
+
+dbWriteTable(con,  "report_citation_traffic_race", df_race,
+             overwrite = TRUE, row.names = FALSE,
+             field.types = charvect)
+
+
+# # write comment to table, and column metadata
+
+table_comment <- paste0("COMMENT ON TABLE report_citation_traffic_race  IS 'Analyzing top 5 traffic citation rates given as a result of a traffic stop within each traffic stop type (moving, nonmoving, equipment)
+for each perceived racial group.
+The denominator (total column) for this analysis is all traffic stops resulting in a citation within each traffic stop type
+for each perceived racial group.
+R script used to recode and import table: W:\\Project\\ECI\\Fresno RIPA\\GitHub\\JZ\\fresnoripa\\Analysis\\report_citation_traffic_type_stop.R
+QA document: W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_report_citation_traffic_type_stop.docx';
+
+COMMENT ON COLUMN report_citation_traffic_race.race IS 'Perceived race';
+COMMENT ON COLUMN report_citation_traffic_race.statute_literal_25 IS 'Text description for accompnaying offense code';
+COMMENT ON COLUMN report_citation_traffic_race.total IS 'Total number (denominator in rate calc) of traffic stops that resulted in a citation within each traffic stop type';
+COMMENT ON COLUMN report_citation_traffic_race.count IS 'Count of each specific citation offense code within each traffic stop type';
+COMMENT ON COLUMN report_citation_traffic_race.rate IS 'Rate of Rate of traffic stops resulting in a citation by each citation code out of all traffic stops resulting in a citation
+within each traffic stop type';
+")
+
+# send table comment + column metadata
+dbSendQuery(conn = con, table_comment)
 
 
